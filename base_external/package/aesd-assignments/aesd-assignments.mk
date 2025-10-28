49295000db07cfc3e7adc304d62209bf848b4ca4
flopezb03 <f.lopezberrocal@gmail.com>
2025-10-28 12:22:04 +0100

    aesd_qemu_defconfig set

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 058676d67bec76331c58d3a3dc7b9126ef8849e8
+AESD_ASSIGNMENTS_VERSION = 7b974be2a7e47a733a6faa5f8fdad2847d81ece1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
